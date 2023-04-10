3d5a25b0bfb13933817b295b57e5a5db82b695fc
Luthfi Aji <luthfi.aji@firstasiaconsultants.tech>
2023-04-10 09:29:07 +0700

    path parameter

--- src/routes.ts
+++ src/routes.ts
@@ -30,6 +30,14 @@ const routes: ServerRoute<ReqRefDefaults>[] = [
         },
     },
     {
+        method: 'GET',
+        path: '/hello/{name?}',
+        handler: (request, h) => {
+            const { name = "stranger" } = request.params;
+            return `Hello, ${name}!`;
+        }
+    },
+    {
         method: '*',
         path: '/{any*}',
         handler: (request, h) => {
